58dec795847c2aa47af9d5cde8cc8a7c38890be8
Yeimss <jamesherrera364@gmail.com>
2025-05-03 18:23:40 -0500

    Se agrega la capa de testeo y s hacer unas pruebas iniciales

---
+++ StringManipulation.Test/StringOperationsTest.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+namespace StringManipulation.Test
+{
+    public class StringOperationsTest
+    {
+        [Fact]
+        public void ConcatenateStrings()
+        {
+            var strOperation = new StringOperations();
+            var result = strOperation.ConcatenateStrings("Hello", "Platzi");
+            Assert.Equal("Hello Platzi", result);
+        }
+        [Fact]
+        public void IsPalindrome_True()
+        {
+            var strOperation = new StringOperations();
+            
+            var result = strOperation.IsPalindrome("seres");
+
+            Assert.True(result);
+        }
+        [Fact]
+        public void IsPalindrome_False()
+        {
+            var strOperation = new StringOperations();
+
+            var result = strOperation.IsPalindrome("Holiiii");
+
+            Assert.False(result);
+        }
+    }
+}
